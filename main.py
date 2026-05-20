29d487aaf7f89739c2852a5524055b7dc3c5f556
Graham Pinkston <graham.pinkston@gmail.com>
2026-05-20 15:03:47 -0500

    full scan first attempt

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from db.write_to_db import insert_file_list
 
 if __name__ == "__main__":
     print("Scanning movies...")
-    rows = scan("movies")
-    print(f"Found: {len(rows)} files")
+    movies = scan("movies")
+    print(f"Found: {len(movies)} files")
 
     print("Writing to DB...")
-    insert_file_list(rows)
+    insert_file_list(movies)
+
+    print("Scanning shows...")
+    shows = scan("shows")
+    print(f"Found: {len(shows)} files")
+
+    print("Writing to DB...")
+    insert_file_list(shows)
